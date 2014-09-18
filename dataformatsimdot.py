72afdd8fabe193bd627f93b8c5fce5477690211e
Adam Struthers <adam@serouslabs.com>
2014-09-18 11:57:11 -0400

    all

--- dataformatsimdot.py
+++ dataformatsimdot.py
@@ -48,42 +48,42 @@ def buildapplist(): ## sheet parser
             maps[rows] = {"riskdomain": riskdomain, "appfromid": appfromid, "appfromfqan": appfromfqan, "apptofqan": apptofqan, "apptoid": apptoid, "functions": functions, "types": types}
 
 buildapplist()
-current = ""
+with open("outputtest.json", "wb") as f:
+    f.write(str(maps))
 
-
-with open("output.json", "wb") as f: ##open output file for writing
-    f.write("[\n") ## write json syntax header
-    k=0 ## app counter
-    for apps in applist: ## iterate through the applications found with initial parsing -- there should only be one row per unique app
-        print apps
-        i=0
-        current = '{"name": "system.app.' + apps + '", "size": 17010, "imports":[]},\n' ## build 0 dependancy row
-        for rows in range(2, 757): ##674
-                for cols in range(3, 4):
-                    try:
-                        print maps[rows]["riskdomain"]
-                    except:
-                        pass
-                    if str(g.cell(rows,4).value) == apps: ## if we found a match to import on a current row
-                        if str(g.cell(rows-1,3).value) == str(g.cell(rows, 3).value): ## if this is a duplicated import then we must BREAK
-                            break
-                        if i == 0: ## also if this is the first iteration after finding an import to add, we must clear the original syntax and rebuild
-                            current = '{"name": "system.app.' + apps + '", "size": 17010, "imports":['
-                        current += '"system.app.' + stripthis(str(g.cell(rows,3).value)) + '",' ## chain import statements to the end of the row
-                        i+=1
-        k+=1
-        if current[-3] != "}": ## weird hacky thing to check if line is terminated correctly
-            s = list(current)
-            s[-1] = ''
-            s.append("]},\n")
-            current = "".join(s)
-        print "k: ", k
-        if k == len(applist): ## if this is the very last app, then we do not need a comma, we need to terminate the row
-            s = list(current)
-            s[-2] = ""
-            current = "".join(s)
-        f.write(current)
-        current = ""
-        if k == len(applist):
-            f.write("\n]") ## terminate the data model syntax
-print "Distinct Apps: ", len(applist)
+# with open("outputtest.json", "wb") as f: ##open output file for writing
+#     f.write("[\n") ## write json syntax header
+#     k=0 ## app counter
+#     for apps in applist: ## iterate through the applications found with initial parsing -- there should only be one row per unique app
+#         print apps
+#         i=0
+#         current = '{"name": "system.app.' + apps + '", "size": 17010, "imports":[]},\n' ## build 0 dependancy row
+#         for rows in range(2, 757): ##674
+#                 for cols in range(3, 4):
+#                     try:
+#                         print maps[rows]["riskdomain"]
+#                     except:
+#                         pass
+#                     if str(g.cell(rows,4).value) == apps: ## if we found a match to import on a current row
+#                         if str(g.cell(rows-1,3).value) == str(g.cell(rows, 3).value): ## if this is a duplicated import then we must BREAK
+#                             break
+#                         if i == 0: ## also if this is the first iteration after finding an import to add, we must clear the original syntax and rebuild
+#                             current = '{"name": "system.app.' + apps + '", "size": 17010, "imports":['
+#                         current += '"system.app.' + stripthis(str(g.cell(rows,3).value)) + '",' ## chain import statements to the end of the row
+#                         i+=1
+#         k+=1
+#         if current[-3] != "}": ## weird hacky thing to check if line is terminated correctly
+#             s = list(current)
+#             s[-1] = ''
+#             s.append("]},\n")
+#             current = "".join(s)
+#         print "k: ", k
+#         if k == len(applist): ## if this is the very last app, then we do not need a comma, we need to terminate the row
+#             s = list(current)
+#             s[-2] = ""
+#             current = "".join(s)
+#         f.write(current)
+#         current = ""
+#         if k == len(applist):
+#             f.write("\n]") ## terminate the data model syntax
+# print "Distinct Apps: ", len(applist)
